4fabe14660b53930301ba0f3581dc33a498b7746
Vincent Boiardt <vincent@boiardt.se>
2011-07-05 12:16:23 +0200

    Added project list after the calendar

--- extension/script.js
+++ extension/script.js
@@ -17,32 +17,61 @@ function main(){
 	jQuery.noConflict();
 	var Colors = {
 		projects: {},
+		
 		init: function() {
+			jQuery('.schedule_wrapper').addClass('clearfix');
+			jQuery('.calendar_link').css('float', 'right');
+			
+			Colors.list = jQuery('<div class="owc-projects clearfix">').insertAfter( jQuery('.calendar_container') );
 			Colors.add_colors();
-			document.body.addEventListener("DOMNodeInserted", Colors.add_colors);
+			
+			var container = jQuery('.calendar_container');
+			container = container[0];
+			container.addEventListener("DOMNodeInserted", Colors.add_colors);
 		},
 		add_colors: function(){
 			jQuery('.calendar_event.spanned').each(function(){
 				var el = jQuery(this);
+				
+				if ( el.hasClass('owc-project-modified') )
+					return;
+					
 				var edit = el.data('editPath');
 				var pattern = /projects\/(\d+)\//;
 				var matches = pattern.exec(edit);
 				if ( matches.length >= 2 ) {
 					var id = matches[1];
-					Colors.projects[id] = id;
-					el.addClass( 'owc-project-' + id );
+					
+					pattern = /Project: (.*)/;
+					matches = pattern.exec( el.find('a.entry_title').attr('title') );
+					
+					var title = '';
+					if ( matches.length >= 2 ) {
+						title = matches[1];
+					}
+					
+					Colors.projects[id] = {
+						id: id,
+						title: title
+					};
+					el.addClass('owc-project-' + id).addClass('owc-project-modified');
 				}
 			});
+			
 			var i = 1;
-			jQuery.each(Colors.projects, function(id){
+			var html = '<ul>';
+			jQuery.each(Colors.projects, function(key, obj){
 				var color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-				jQuery('.owc-project-' + id).addClass('owc-project-color-' + i );
+				jQuery('.owc-project-' + obj.id).addClass('owc-project-color-' + i);
+				html += '<li class="owc-project-color-' + i + '">' + obj.title + '</li>';
 
 				if ( i == 10 )
 					i = 0;
 	
 				i++;
 			});
+			html += '</ul>';
+			Colors.list.html(html);
 		}
 	};
 	Colors.init();
